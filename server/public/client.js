0903e4d6b2639e9ea2df9979a0442699202b2212
Gabin Williams <gabinwilliams@gmail.com>
2020-12-18 23:40:10 +0100

    added express and setup all needed static files for project.  Built out html for project.

---
+++ server/public/client.js
@@ -0,0 +1,9 @@
+
+console.log('Js Ready!');
+
+$(document).ready(handleReady);
+
+function handleReady() {
+
+  console.log('jQuery ready!');
+}// end handleReady
---
+++ server/server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static('server/public'));
+
+
+
+
+
+
+
+
+// START SERVER!
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+})
