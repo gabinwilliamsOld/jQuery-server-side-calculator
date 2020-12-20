1dc7fc97f854ded62a2c36ddb5d963797f05c2ff
Gabin Williams <gabinwilliams@gmail.com>
2020-12-20 11:45:58 +0100

    formated server.js and added comments

--- server/server.js
+++ server/server.js
@@ -1,133 +1,119 @@
-
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = 5000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
 let equationData = [];
 
-const serverData =[];
+const serverData = [];
 
 // GET ROUTE
 
 app.get("/data", (req, res) => {
   console.log("you got to /calculate!");
   console.log(serverData);
-  // response is to send inventory array
-  
+  // response is to send serverData array to client side
   res.send(serverData);
-
 });
-
 
 app.get("/calculate", (req, res) => {
   console.log("you got to /calculate!");
   console.log(equationData);
-  // response is to send inventory array
-  
+
+  // pushes the one object in equationData into the const array serverData
   serverData.push(equationData[0]);
 
   res.send(equationData);
-  
+  // empties equationData after sending to client side
   equationData = [];
-
 });
 
 // POST ROUTE
 app.post("/calculate", (req, res) => {
-  // the REQUEST needs to be able to receive data
-  let equationObject = req.body; // .body is coming from body parser
- 
-  // pushes newItem into inventory array
-  // equationData.push(equationObject);
+  let equationObject = req.body;
+
   console.log(equationObject);
+  // runs handleEquation function with equationObject data
   handleEquation(equationObject);
 
   // 201 code means created
   res.sendStatus(201);
 });
 
-// FUNCTIONS
-
+// function to complete all math operations
 function handleEquation(equationObject) {
-
   let numberValueOne = equationObject.valueOne;
   let numberValueTwo = equationObject.valueTwo;
-  console.log('valueOne and valueTwo:', numberValueOne, numberValueTwo);
+  console.log("valueOne and valueTwo:", numberValueOne, numberValueTwo);
 
-  if(equationObject.operator === 'plus') {
+  if (equationObject.operator === "plus") {
     let result = Number(numberValueOne) + Number(numberValueTwo);
-    console.log('In add:', result);
+    console.log("In add:", result);
 
     let resultObject = {
       valueOne: numberValueOne,
       valueTwo: numberValueTwo,
-      operator: '+',
-      outcome: result
-    }
+      operator: "+",
+      outcome: result,
+    };
 
     equationData.push(resultObject);
 
     return result;
-
-  }else if (equationObject.operator === 'minus') {
+  } else if (equationObject.operator === "minus") {
     let result = Number(numberValueOne) - Number(numberValueTwo);
-    console.log('In minus:', result);
+    console.log("In minus:", result);
 
     let resultObject = {
       valueOne: numberValueOne,
       valueTwo: numberValueTwo,
-      operator: '-',
-      outcome: result
-    }
+      operator: "-",
+      outcome: result,
+    };
 
     equationData.push(resultObject);
 
     return result;
-
-  }else if (equationObject.operator === 'times') {
+  } else if (equationObject.operator === "times") {
     let result = Number(numberValueOne) * Number(numberValueTwo);
-    console.log('In times:', result);
+    console.log("In times:", result);
 
     let resultObject = {
       valueOne: numberValueOne,
       valueTwo: numberValueTwo,
-      operator: '*',
-      outcome: result
-    }
+      operator: "*",
+      outcome: result,
+    };
 
     equationData.push(resultObject);
 
     return result;
-
-  }else if (equationObject.operator === 'divide') {
+  } else if (equationObject.operator === "divide") {
     let result = Number(numberValueOne) / Number(numberValueTwo);
-    console.log('In divide', result);
+    console.log("In divide", result);
 
     let resultObject = {
       valueOne: numberValueOne,
       valueTwo: numberValueTwo,
-      operator: '/',
-      outcome: result
-    }
+      operator: "/",
+      outcome: result,
+    };
 
     equationData.push(resultObject);
 
     return result;
-
-  }else{
-    console.log('In handleEquation. This doesnt work!');
+  } else {
+    console.log("In handleEquation. This doesnt work!");
     return false;
   }
-}// end equation
-
+} // end equation
 
 // START SERVER!
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
